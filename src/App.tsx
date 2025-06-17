701e462a483aca467aa27b764b4ad9413d0face0
anunyaOrange <anunyaOrange@proton.me>
2025-06-17 21:05:28 +0700

    add calendar

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
 
 function App() {
   const [count, setCount] = useState(0)
+  const [date, setDate] = useState<Date | undefined>(new Date())
 
   return (
     <>
+      <div className="flex flex-col items-center justify-center space-y-4">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          className="rounded-md border shadow-sm"
+          captionLayout="dropdown"
+        />
+        <Button>Click me</Button>
+        <Button>Click me</Button>
+      </div>
       <div className="flex min-h-svh flex-col items-center justify-center">
         <Button>Click me</Button>
       </div>
