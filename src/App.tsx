550fdd3004713c841d3169ea4e73a7143da06b77
anunyaOrange <anunyaOrange@proton.me>
2025-06-18 16:38:53 +0700

    temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { ThemeProvider } from "@/components/theme-provider"
+import { ModeToggle } from "@/components/mode-toggle"
 import { Button } from "@/components/ui/button"
 import { Calendar } from "@/components/ui/calendar"
+
 
 function App() {
   const [count, setCount] = useState(0)
   const [date, setDate] = useState<Date | undefined>(new Date())
 
   return (
-    <>
-      <div className="flex flex-col items-center justify-center space-y-4">
+    <ThemeProvider defaultTheme="dark" storageKey="theme">
+
+      <div className="flex flex-col items-center justify-center space-y-4 max-w-[300px] mx-auto">
+        <div className="bg-color-primary w-full">
+          <div className="relative top-4 right-4 z-50">
+            <ModeToggle />
+          </div>
+        </div>
         <Calendar
           mode="single"
           selected={date}
@@ -22,30 +29,7 @@ function App() {
         <Button>Click me</Button>
         <Button>Click me</Button>
       </div>
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button>Click me</Button>
-      </div>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>cursorshadcn</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </ThemeProvider>
   )
 }
 
